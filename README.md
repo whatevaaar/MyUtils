6a079f5db36d61becb1a27294ef3dc426cf16e3e
Whatevaaar <emilianohidalgo314@gmail.com>
2019-04-09 21:41:15 -0500

    Updt readme/

---
+++ README.md
@@ -0,0 +1,5 @@
+#MyUtils
+Utilidades y pequeños scripts para Linux
+
+##Extractor
+Script para extraer todos los archivos de un directorio de directorios**n
--- extractor.py
+++ extractor.py
@@ -5,9 +5,7 @@ import sys
 
 #Signo para los directorios dependiendo del so
 signoDir = '/' if (os.name == "posix") else '\\'
-directorio = os.getcwd() if len(sys.argv) < 2  else sys.argv[1:]
-print(directorio)
+directorio = os.getcwd()
 archivos = [x for x in glob.glob(directorio + signoDir + "**" + signoDir + "*.*")]
 for archivo in archivos:
     os.rename(archivo, directorio + signoDir + os.path.basename(archivo))
-print(archivos)
