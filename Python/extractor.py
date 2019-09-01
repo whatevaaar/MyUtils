f297a63212ededb916f0c41422a9ba9098d62e3a
Your Name <emiliano_hc@hotmail.com>
2019-09-01 15:23:00 -0500

    Fix extractor

--- Python/extractor.py
+++ Python/extractor.py
@@ -6,6 +6,8 @@ import sys
 #Signo para los directorios dependiendo del so
 signoDir = '/' if (os.name == "posix") else '\\'
 directorio = os.getcwd()
-archivos = [x for x in glob.glob(directorio + signoDir + "**" + signoDir + "*.*")]
+archivos = [x for x in glob.glob(directorio + signoDir + "**" + signoDir + "*.*",recursive=True)]
+
 for archivo in archivos:
     os.rename(archivo, directorio + signoDir + os.path.basename(archivo))
+
